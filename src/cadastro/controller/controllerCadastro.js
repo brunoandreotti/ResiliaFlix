b647d7678dfbd951044f758230f0527aceb4bb71
Bruno Andreotti <brunoandreotti@outlook.com>
2021-12-23 14:59:19 -0300

    validacao campos cadastro

--- src/cadastro/controller/controllerCadastro.js
+++ src/cadastro/controller/controllerCadastro.js
@@ -5,11 +5,24 @@ let rua = $('#rua')
 let bairro = $('#bairro')
 let cidade = $('#cidade')
 let estado = $('#uf')
-let cepsmall = $('#cepHelp')
+let cepSmall = $('#cepHelp')
+let cpfInput = $('#cpf')
+let cpfSmall = $('#cpfHelp')
+let email = $('#email')
+let emailSmall = $('#emailHelp')
+let confirmaEmail = $('#emailConfirm')
+let confirmaEmailSmall = $('#emailConfirmHelp')
+let senha = $('#senha')
+let confirmaSenha = $('#senhaConfirm')
+let confirmaSenhaSmall = $('#senhaConfirmHelp')
 
 // EVENTS
 
 cep.on('blur', funCEP)
+cpfInput.on('blur', validaCPF)
+email.on('blur', validaEmail)
+confirmaEmail.on('blur', ViewCadastro.validarConfimaEmail)
+confirmaSenha.on('blur', ViewCadastro.validarConfirmaSenha)
 
 // FUNCTIONS
 
@@ -18,13 +31,21 @@ async function funCEP() {
   try {
     let dadosCep = await ModelCadastro.consultaCep(cepValor)
     if (dadosCep.erro) {
-      cepsmall.text('CEP não encontrado!')
+      cepSmall.text('CEP não encontrado!')
       ViewCadastro.limpaCamposCEP()
     } else {
       ViewCadastro.completaCamposCEP(dadosCep)
     }
   } catch {
-    cepsmall.text('Valor do CEP inválido!')
+    cepSmall.text('Valor do CEP inválido!')
     ViewCadastro.limpaCamposCEP()
   }
+}
+
+function validaCPF() {
+  ViewCadastro.validarCPF(cpfInput.val())
+}
+
+function validaEmail() {
+  ViewCadastro.validarEmail(email.val())
 }
